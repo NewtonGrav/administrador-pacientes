1284f9e23824275173171e387d9005791d07c968
Gonzalo <gonzalom97@outlook.com>
2020-10-02 09:42:49 -0300

    git init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './index.css'
+
+function App() {
+	return (
+		<Fragment>
+			<h1>Adminsitrador de pacientes</h1>
+		</Fragment>
+	);
+}
+
+export default App;
