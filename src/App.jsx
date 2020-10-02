6b3c0e6876c1a771b65d8f407325279ca0d5b15b
Gonzalo <gonzalom97@outlook.com>
2020-10-02 09:53:57 -0300

    style: Estructura base para App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React, { Fragment } from 'react';
-import './index.css'
+import './index.css';
+import Formulario from './components/Formulario';
 
 function App() {
 	return (
 		<Fragment>
 			<h1>Adminsitrador de pacientes</h1>
+
+			<div className='container'>
+				<div className='row'>
+					<div className='six columns'>
+						<Formulario />
+					</div>
+					<div className='six columns'>2</div>
+				</div>
+			</div>
 		</Fragment>
 	);
 }
