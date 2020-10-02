cc0ec8b3e883cc553b2ebbaf0dc1d3768fb848b6
Gonzalo <gonzalom97@outlook.com>
2020-10-02 10:17:32 -0300

    feat: Se agrego campos al formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,8 +6,40 @@ const Formulario = () => {
 			<h2>Crear Cita</h2>
 
 			<form>
-				<label htmlFor='nombre-mascota'>Nombre de mascota</label>
-				<input id='nombre-mascota' type='text' name='mascota' />
+				<label>Nombre de mascota</label>
+				<input
+					className='u-full-width'
+					placeholder='Nombre mascota'
+					type='text'
+					name='mascota'
+				/>
+
+				<label>Nombre del dueño</label>
+				<input
+					className='u-full-width'
+					placeholder='Nombre dueño'
+					type='text'
+					name='duenio'
+				/>
+
+				<div className='row'>
+					<div className='six columns'>
+						<label>Fecha</label>
+						<input className='u-full-width' type='date' name='duenio' />
+					</div>
+
+					<div className='six columns'>
+						<label>Hora</label>
+						<input className='u-full-width' type='time' name='duenio' />
+					</div>
+				</div>
+
+				<label>Sintomas</label>
+				<textarea className='u-full-width' name='sintomas'></textarea>
+
+				<button className='button button-primary u-full-width' type='submit'>
+					Agregar cita
+				</button>
 			</form>
 		</Fragment>
 	);
