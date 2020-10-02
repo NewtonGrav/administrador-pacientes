9998fa7c4e6733a8a722b22dfbc556bd21a5ddf9
Gonzalo <gonzalom97@outlook.com>
2020-10-02 11:02:39 -0300

    feat: Bind state cita con los campos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,12 +2,22 @@ import React, { Fragment, useState } from 'react';
 
 const Formulario = () => {
 	const [cita, setCita] = useState({
-		nombre: '',
+		mascota: '',
 		propietario: '',
-		fecha: null,
+		fecha: '',
 		hora: '',
-		sintomas: ''
-	})
+		sintomas: '',
+	});
+
+	const handlerState = (e) => {
+		console.log('Escribiendo...', e.target.name);
+		setCita({
+			...cita,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const { mascota, propietario, fecha, hora, sintomas } = cita;
 
 	return (
 		<Fragment>
@@ -17,33 +27,54 @@ const Formulario = () => {
 				<label>Nombre de mascota</label>
 				<input
 					className='u-full-width'
+					value={mascota}
 					placeholder='Nombre mascota'
 					type='text'
 					name='mascota'
+					onChange={handlerState}
 				/>
 
 				<label>Nombre del dueño</label>
 				<input
 					className='u-full-width'
+					value={propietario}
 					placeholder='Nombre dueño'
 					type='text'
 					name='propietario'
+					onChange={handlerState}
 				/>
 
 				<div className='row'>
 					<div className='six columns'>
 						<label>Fecha</label>
-						<input className='u-full-width' type='date' name='fecha' />
+						<input
+							className='u-full-width'
+							value={fecha}
+							type='date'
+							name='fecha'
+							onChange={handlerState}
+						/>
 					</div>
 
 					<div className='six columns'>
 						<label>Hora</label>
-						<input className='u-full-width' type='time' name='hora' />
+						<input
+							className='u-full-width'
+							value={hora}
+							type='time'
+							name='hora'
+							onChange={handlerState}
+						/>
 					</div>
 				</div>
 
 				<label>Sintomas</label>
-				<textarea className='u-full-width' name='sintomas'></textarea>
+				<textarea
+					className='u-full-width'
+					value={sintomas}
+					name='sintomas'
+					onChange={handlerState}
+				></textarea>
 
 				<button className='button button-primary u-full-width' type='submit'>
 					Agregar cita
